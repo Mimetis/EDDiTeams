cf469da01d1ea975396f6d32485cbaa9c58c359f
mimetis <spertus@microsoft.com>
2018-05-21 18:23:35 +0200

    switching to NG6

--- routes/config.ts
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req: any, res: any, next: any) {
-    res.render('config', { title: 'MS Teams Application' });
-});
-
-module.exports = router;
--- routes/edditeams.ts
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function (req: any, res: any, next: any) {
-    res.render('edditeams', { title: 'MS Teams Application' });
-});
-
-module.exports = router;
